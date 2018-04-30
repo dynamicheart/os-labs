3eb258b6040beeef3b5f9313e6acf033d8531c6c
Jianbang Yang <yangjianbang112@gmail.com>
2018-04-30 22:56:29 +0800

    Lab4: Finish page-fault handling of part B

--- kern/syscall.c
+++ kern/syscall.c
@@ -152,7 +152,15 @@ static int
 sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
 	// LAB 4: Your code here.
-	panic("sys_env_set_pgfault_upcall not implemented");
+	int res;
+	struct Env *env_store;
+
+	if ((res = envid2env(envid, &env_store, 1)) < 0)
+		return res;
+
+	env_store->env_pgfault_upcall = func;
+
+	return 0;
 }
 
 // Allocate a page of memory and map it at 'va' with permission
@@ -419,6 +427,7 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 			res = sys_env_set_status((envid_t)a1, (int)a2);
 			break;
 		case SYS_env_set_pgfault_upcall:
+			res = sys_env_set_pgfault_upcall((envid_t)a1, (void *)a2);
 			break;
 		case SYS_yield:
 			asm volatile("movl 32(%%ebp), %0":"=r"(curenv->env_tf.tf_eip):);
--- kern/trap.c
+++ kern/trap.c
@@ -1,6 +1,7 @@
 #include <inc/mmu.h>
 #include <inc/x86.h>
 #include <inc/assert.h>
+#include <inc/string.h>
 
 #include <kern/pmap.h>
 #include <kern/trap.h>
@@ -341,6 +342,26 @@ page_fault_handler(struct Trapframe *tf)
 	//   (the 'tf' variable points at 'curenv->env_tf').
 
 	// LAB 4: Your code here.
+	if (curenv->env_pgfault_upcall) {
+		struct UTrapframe utf;
+		utf.utf_fault_va = fault_va;
+		utf.utf_err = tf->tf_err;
+		utf.utf_regs = tf->tf_regs;
+		utf.utf_eip = tf->tf_eip;
+		utf.utf_eflags = tf->tf_eflags;
+		utf.utf_esp = tf->tf_esp;
+
+		tf->tf_eip = (uintptr_t)curenv->env_pgfault_upcall;
+		if (tf->tf_esp >= UXSTACKTOP - PGSIZE && tf->tf_esp < UXSTACKTOP)
+			tf->tf_esp = tf->tf_esp - sizeof(uint32_t) - sizeof(struct UTrapframe);
+		else
+			tf->tf_esp = UXSTACKTOP - sizeof(struct UTrapframe);
+
+		user_mem_assert(curenv, (void *)tf->tf_esp, sizeof(struct UTrapframe), PTE_W);
+		memmove((void *)tf->tf_esp, &utf, sizeof(struct UTrapframe));
+
+		env_run(curenv);
+	}
 
 	// Destroy the environment that caused the fault.
 	cprintf("[%08x] user fault va %08x ip %08x\n",
